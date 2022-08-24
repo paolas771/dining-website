0a57235e017d249bf47d11bce957c82a68f221e3
Paola Silva <paolas771@gmail.com>
2022-08-23 19:35:36 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,11 +5,7 @@ import HomePage from './Pages/HomePage'
 export default function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      dine
-=======
       <HomePage />
->>>>>>> 1d0e526 (add header background and scss variables)
     </div>
   );
 }

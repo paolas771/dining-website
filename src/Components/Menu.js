7e1ac8f666963bca629672372f2031923a5b7c46
Paola Silva <paolas771@gmail.com>
2022-09-16 20:03:04 -0500

    finish styling menu u section

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -3,7 +3,7 @@ export default function Menu(props){
     return(
         <div className="item">
             <img className="menuPic" src={props.img} alt=""/>
-            {props.width >= 575 && <hr className="menuLine"></hr>}
+            {props.width >= 575 && <img src="../images/patterns/pattern-divide.svg" alt="" className="menuLine"/>}
             <h4 className="menuItem">{props.item}</h4>
             <p className="menuDes">{props.description}</p>
         </div>
